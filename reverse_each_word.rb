029242dead8ee1fe35e94b5a533d23d2c3ab5892
Saima Akhtar <saimaakhtar@gmail.com>
2020-04-09 17:13:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
-def reverse_each_word(sentence)
-  sentence.reverse.split.reverse.join(" ")
- end
+#def reverse_each_word(sentence)
+ # sentence.reverse.split.reverse.join(" ")
+ #end
  
+#def reverse_each_word(sentence)
+# sentence.split.collect {|word| word.reverse}.join(" ")
+#end
+
 def reverse_each_word(sentence)
- sentence.split.collect {|word| word.reverse}.join(" ")
+  sentence.split {|i| i.reverse}
 end
